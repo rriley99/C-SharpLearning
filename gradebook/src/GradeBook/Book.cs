ec97aa62ae2940998b20d772f7e9312c6a7c7085
Robert Riley <rriley99@gmail.com>
2020-11-02 16:00:59 -0500

    Added console input operations

--- gradebook/src/GradeBook/Book.cs
+++ gradebook/src/GradeBook/Book.cs
@@ -53,15 +53,37 @@ namespace GradeBook
             result.High = double.MinValue;
             result.Low = double.MaxValue;
             
-            for(var index = 0; index < grades.Count; index +=1)
-            {                
-                result.High = Math.Max(grades[index], result.High);
-                result.Low = Math.Min(grades[index], result.Low);
-                result.Average += grades[index]; 
-                index += 1;   
+            foreach(var grade in grades)
+            {
+                result.Low = Math.Min(grade, result.Low);
+                result.High = Math.Max(grade, result.High);
+                result.Average += grade;
             } 
             result.Average /= grades.Count;
             
+            switch(result.Average)
+            {
+                case var d when d >= 90.0:
+                    result.Letter = 'A';
+                    break;
+
+                case var d when d >= 80.0:
+                    result.Letter = 'B';
+                    break;
+
+                case var d when d >= 70.0:
+                    result.Letter = 'C';
+                    break;
+
+                case var d when d >= 60.0:
+                    result.Letter = 'D';
+                    break;
+
+                default:
+                    result.Letter = 'F';
+                    break;
+
+            }
             return result;
         }
         private List<double> grades;
--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -8,15 +8,27 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {   
-            var book = new Book("Robert's Grade Book");
-            book.AddGrade(89.1);
-            book.AddGrade(90.5);
-            book.AddGrade(77.5);
+            var book = new Book("Robert's Grade Book");       
 
+            while (true)
+            {   
+                Console.WriteLine("Please enter a grade (or type complete to finish adding grades).");
+                var input = Console.ReadLine();
+
+                if(input == "complete")
+                {
+                   break;
+                }
+                
+                var grade = double.Parse(input);
+                book.AddGrade(grade);
+            }
+            
             var stats = book.GetStatistics();
             System.Console.WriteLine($"The max grade is: {stats.High}");
             System.Console.WriteLine($"The min grade is: {stats.Low}");
             System.Console.WriteLine($"The avg grade is: {stats.Average}");
+            System.Console.WriteLine($"The letter grade is: {stats.Letter}");
         }       
     }
 }
